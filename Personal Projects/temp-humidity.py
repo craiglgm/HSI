69a4fdea72d161b83fbf38557a7d63e608c13ad7
craiglgm <33362397+craiglgm@users.noreply.github.com>
2018-07-23 15:17:34 -0400

    -

--- Personal Projects/temp-humidity.py
+++ Personal Projects/temp-humidity.py
@@ -1,6 +1,14 @@
-# A simple program for my Relay Pi
-# Controls four outlets, and reads temperature and humidity in Shedquarters.
+""" 
+A simple program for my Relay Pi
+Controls four outlets, and reads temperature and humidity in Shedquarters.
+Runs every 15 minutes to update the chart
+"""
 
 # Configuration Settings
+html_file_path = 'c:\\users\\Craig\\Downloads\\'
+html_file_name = 'shedquarters.html'
+
+def main():
+	print( html_file_path + html_file_name )
 
 main()
